fddcaf38137e7d905ceb83d81386bbeab3563037
Brian Ndirangu <ndikraxx@gmail.com>
2018-11-20 22:34:41 +0300

    init

---
+++ app/src/main/java/com/attendancesystem/Admin_homeActivity.java
@@ -0,0 +1,35 @@
+package com.attendancesystem;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Admin_homeActivity extends Activity {
+	 @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.admin_page); 
+	 }
+	 
+	 public void manage_stud(View v)
+	 {
+		 Intent i=new Intent(this,StudentActivity.class);
+		
+         startActivity(i);
+	 }
+	 public void manage_fac(View v)
+	 {
+		 Intent i=new Intent(this,FactultyActivity.class);
+		 
+         startActivity(i);
+	 }
+
+	 public void log_out(View v)
+	 {
+		 Intent i=new Intent(this,MainAd.class);
+
+         startActivity(i);
+	 }
+
+}
---
+++ app/src/main/java/com/attendancesystem/TestActivity.java
@@ -0,0 +1,39 @@
+package com.attendancesystem;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class TestActivity extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manage);
+	}
+	public void log_out(View v)
+	{
+		
+		Intent i = new Intent(this,MainAd.class);
+		finish();
+		startActivity(i);
+	}
+	public void manage_stud(View v)
+	{
+		Intent i = new Intent(this,ManageActivity.class);
+		finish();
+		startActivity(i);
+	}
+	public void mark_att(View v)
+	{
+		Intent i = new Intent(this,AttendanceActivity.class);
+		startActivity(i);
+	}
+	public void view_att(View v)
+	{
+		Intent i = new Intent(this,ViewActivity.class);
+		startActivity(i);
+	}
+		
+}
+
